fc3a662b635f26708b54d2866c32d9497a0a191d
Shelly Kagan <skagan@redhat.com>
2023-12-03 13:07:58 +0200

    Lint fixes
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- pkg/internal/launcher/launcher_test.go
+++ pkg/internal/launcher/launcher_test.go
@@ -48,14 +48,30 @@ func TestLauncherRunShouldFailWhen(t *testing.T) {
 		reporter reporterStub
 		errors   []string
 	}{
-		"report fails":                  {checkup: checkupStub{}, reporter: reporterStub{failReport: errReport}, errors: []string{errReport.Error()}},
-		"setup fails":                   {checkup: checkupStub{failSetup: errSetup}, reporter: reporterStub{}, errors: []string{errSetup.Error()}},
-		"run fails":                     {checkup: checkupStub{failRun: errRun}, reporter: reporterStub{}, errors: []string{errRun.Error()}},
-		"teardown fails":                {checkup: checkupStub{failTeardown: errTeardown}, reporter: reporterStub{}, errors: []string{errTeardown.Error()}},
-		"setup and 2nd report fail":     {checkup: checkupStub{failSetup: errSetup}, reporter: reporterStub{failReport: errReport, failOnSecondReport: true}, errors: []string{errSetup.Error(), errReport.Error()}},
-		"run and report fail":           {checkup: checkupStub{failRun: errRun}, reporter: reporterStub{failReport: errReport, failOnSecondReport: true}, errors: []string{errRun.Error(), errReport.Error()}},
-		"teardown and report fail":      {checkup: checkupStub{failTeardown: errTeardown}, reporter: reporterStub{failReport: errReport, failOnSecondReport: true}, errors: []string{errTeardown.Error(), errReport.Error()}},
-		"run, teardown and report fail": {checkup: checkupStub{failRun: errRun, failTeardown: errTeardown}, reporter: reporterStub{failReport: errReport, failOnSecondReport: true}, errors: []string{errRun.Error(), errTeardown.Error(), errReport.Error()}},
+		"report fails": {checkup: checkupStub{},
+			reporter: reporterStub{failReport: errReport},
+			errors:   []string{errReport.Error()}},
+		"setup fails": {checkup: checkupStub{failSetup: errSetup},
+			reporter: reporterStub{},
+			errors:   []string{errSetup.Error()}},
+		"run fails": {checkup: checkupStub{failRun: errRun},
+			reporter: reporterStub{},
+			errors:   []string{errRun.Error()}},
+		"teardown fails": {checkup: checkupStub{failTeardown: errTeardown},
+			reporter: reporterStub{},
+			errors:   []string{errTeardown.Error()}},
+		"setup and 2nd report fail": {checkup: checkupStub{failSetup: errSetup},
+			reporter: reporterStub{failReport: errReport, failOnSecondReport: true},
+			errors:   []string{errSetup.Error(), errReport.Error()}},
+		"run and report fail": {checkup: checkupStub{failRun: errRun},
+			reporter: reporterStub{failReport: errReport, failOnSecondReport: true},
+			errors:   []string{errRun.Error(), errReport.Error()}},
+		"teardown and report fail": {checkup: checkupStub{failTeardown: errTeardown},
+			reporter: reporterStub{failReport: errReport, failOnSecondReport: true},
+			errors:   []string{errTeardown.Error(), errReport.Error()}},
+		"run, teardown and report fail": {checkup: checkupStub{failRun: errRun, failTeardown: errTeardown},
+			reporter: reporterStub{failReport: errReport, failOnSecondReport: true},
+			errors:   []string{errRun.Error(), errTeardown.Error(), errReport.Error()}},
 	}
 
 	for name, tc := range tests {
